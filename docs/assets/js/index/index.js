ff5ff65479592883d7fff2fc03888c51a2b2a75b
Dave Gandy <dave@davegandy.com>
2012-05-21 09:06:58 -0400

    ie7 more fully supported, large refactor of styles

--- docs/assets/js/index/index.js
+++ docs/assets/js/index/index.js
@@ -5,14 +5,14 @@ $(function() {
         modalTemplate: _.template($("#modal-template").html()),
 
         events:{
-            "click ul.the-icons > li": "iconClicked"
+            "click ul.ficons > li": "iconClicked"
         },
 
         iconClicked: function(event) {
             event.preventDefault();
 
             var $item = $(event.currentTarget);
-            var $modal = $(this.modalTemplate({"style": $item.attr("class")}));
+            var $modal = $(this.modalTemplate({"style": $item.find("i").attr("class")}));
 
             $modal.modal("show");
             $modal.on('hidden', function () {
