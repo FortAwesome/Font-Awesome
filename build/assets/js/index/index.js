fd076658883b6514052201f2e337e0f48c187bcb
davegandy <dave@davegandy.com>
2013-05-02 20:12:33 -0400

    massive update to doc structure & build

---
+++ build/assets/js/index/index.js
@@ -0,0 +1,27 @@
+$(function() {
+  // start the icon carousel
+  $('#iconCarousel').carousel({
+    interval: 5000
+  });
+
+  // make code pretty
+  window.prettyPrint && prettyPrint();
+
+  // inject twitter & github counts
+  $.ajax({
+    url: 'http://api.twitter.com/1/users/show.json',
+    data: {screen_name: 'fortaweso_me'},
+    dataType: 'jsonp',
+    success: function(data) {
+      $('#followers').html(data.followers_count);
+    }
+  });
+  $.ajax({
+    url: 'https://api.github.com/repos/fortawesome/Font-Awesome',
+    dataType: 'jsonp',
+    success: function(data) {
+      $('#watchers').html(data.data.watchers);
+      $('#forks').html(data.data.forks);
+    }
+  });
+});
