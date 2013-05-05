3c10a7964ab9f98b8ad9be30f56432850fc88c78
davegandy <dave@davegandy.com>
2013-05-04 21:41:26 -0400

    fixing #721

--- build/assets/js/index/index.js
+++ build/assets/js/index/index.js
@@ -8,21 +8,21 @@ $(function() {
   $('pre').addClass('prettyprint');
   window.prettyPrint && prettyPrint();
 
-  // inject twitter & github counts
-  $.ajax({
-    url: 'http://api.twitter.com/1/users/show.json',
-    data: {screen_name: 'fortaweso_me'},
-    dataType: 'jsonp',
-    success: function(data) {
-      $('#followers').html(data.followers_count);
-    }
-  });
-  $.ajax({
-    url: 'https://api.github.com/repos/fortawesome/Font-Awesome',
-    dataType: 'jsonp',
-    success: function(data) {
-      $('#watchers').html(data.data.watchers);
-      $('#forks').html(data.data.forks);
-    }
-  });
+//  // inject twitter & github counts
+//  $.ajax({
+//    url: 'http://api.twitter.com/1/users/show.json',
+//    data: {screen_name: 'fortaweso_me'},
+//    dataType: 'jsonp',
+//    success: function(data) {
+//      $('#followers').html(data.followers_count);
+//    }
+//  });
+//  $.ajax({
+//    url: 'https://api.github.com/repos/fortawesome/Font-Awesome',
+//    dataType: 'jsonp',
+//    success: function(data) {
+//      $('#watchers').html(data.data.watchers);
+//      $('#forks').html(data.data.forks);
+//    }
+//  });
 });
