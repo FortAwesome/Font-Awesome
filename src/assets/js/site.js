2828f081dc0e7efc9fa555f6dd0f0081e1c8cabb
Mottie <wowmotty@gmail.com>
2013-11-11 22:32:02 -0600

    Sortable cheatsheet
    Added a sortable/filterable cheatsheet for easier reference

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -4,7 +4,43 @@ $(function() {
     interval: 5000
   });
 
-
+  if ($("table.cheatsheet").length) {
+    $.extend($.tablesorter.themes.bootstrap, {
+      icons    : "fa fa-inverse",
+      sortAsc  : "fa-chevron-up",
+      sortDesc : "fa-chevron-down"
+    });
+    $("table.cheatsheet").tablesorter({
+      theme: "bootstrap",
+      headerTemplate: "{content}{icon}",
+      widgets: ["zebra", "uitheme", "filter"],
+      widgetOptions: {
+        filter_functions: {
+          // add category select options
+          3: {
+            "Brand Icons"           : function(txt) { return /Brand/.test(txt); },
+            "Currency Icons"        : function(txt) { return /Currency/.test(txt); },
+            "Directional Icons"     : function(txt) { return /Directional/.test(txt); },
+            "Text Editor Icons"     : function(txt) { return /Editor/.test(txt); },
+            "Form Control Icons"    : function(txt) { return /Form/.test(txt); },
+            "Medical Icons"         : function(txt) { return /Medical/.test(txt); },
+            "Video Player Icons"    : function(txt) { return /Video/.test(txt); },
+            "Web Application Icons" : function(txt) { return /Web/.test(txt); }
+          }
+        }
+      },
+      textExtraction: {
+        0 : function(node) {
+          // convert hex to decimal
+          return parseInt( $(node).attr("data-id"), 16 );
+        },
+        2 : function(node){
+          // convert unicode hex to decimal
+          return parseInt( $(node).text().replace(/&#x/,""), 16 );
+        }
+      }
+    });
+  }
 
 
   // make code pretty
