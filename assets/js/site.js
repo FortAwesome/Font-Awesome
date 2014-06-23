25844f89e9f1f6aaff09bf27a5ac6db524d00efa
Dave Gandy <dave@davegandy.com>
2014-06-23 16:14:51 -0500

    updating random messages for kickstarter

--- assets/js/site.js
+++ assets/js/site.js
@@ -5,8 +5,6 @@ $(function() {
   });
 
   var quotes = new Array(
-      "Please support <strong>Black Tie</strong>, the new icon font from Font Awesome!",
-      "Please help me make Font Awesome <strong>Black Tie</strong>!",
       "Get your company logo added to Font Awesome!",
       "Need a specific icon added to Font Awesome?",
       "If you like Font Awesome, you're going to love <strong>Black Tie</strong>!"
