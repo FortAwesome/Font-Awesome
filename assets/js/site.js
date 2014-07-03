3941b3b9f164a5bd06a7797c994d38026b284067
Dave Gandy <dave@davegandy.com>
2014-07-03 19:56:36 -0400

    updating kickstarter messages, updating spinning icon examples link

--- assets/js/site.js
+++ assets/js/site.js
@@ -6,6 +6,9 @@ $(function() {
 
   var quotes = new Array(
       "Get your company logo added to Font Awesome!",
+      "Want SVG support in Font Awesome? Support the Kickstarter!",
+      "Major improvements to Font Awesome at 3000 backers!",
+      "Better Font Awesome icon search at 3000 backers!",
       "Need a specific icon added to Font Awesome?",
       "If you like Font Awesome, you're going to love <strong>Black Tie</strong>!"
     ),
