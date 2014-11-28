3c302e861a25b35e83db6b02a544d18079a734d2
Dan Dascalescu <ddascalescu+github@gmail.com>
2014-11-28 14:32:00 -0800

    Update comment for EOT re. IE9+

--- meteor/package.js
+++ meteor/package.js
@@ -8,7 +8,7 @@ var packageJson = JSON.parse(Npm.require("fs").readFileSync('package.json'));
 Package.describe({
   name: packageName,
   summary: 'Font Awesome (official): 470+ scalable vector icons, customizable via CSS, Retina friendly',
-  version: packageJson.version,
+  version: packageJson.version + '_2',
   git: 'https://github.com/FortAwesome/Font-Awesome.git'
 });
 
@@ -16,7 +16,7 @@ Package.onUse(function (api) {
   api.versionsFrom('METEOR@0.9.2.1');
   api.addFiles([
     // we bundle all font files, but the client will request only one of them via the CSS @font-face rule
-    'fonts/fontawesome-webfont.eot',  // IE8 or older
+    'fonts/fontawesome-webfont.eot',  // IE8 or older only understands EOT. IE9+ will read it too because it loads the first occurrence of `src`
     'fonts/fontawesome-webfont.svg',  // SVG fallback for iOS < 5 - http://caniuse.com/#feat=svg-fonts, http://stackoverflow.com/a/11002874/1269037
     'fonts/fontawesome-webfont.ttf',  // Android Browers 4.1, 4.3 - http://caniuse.com/#feat=ttf
     'fonts/fontawesome-webfont.woff', // Most modern browsers
