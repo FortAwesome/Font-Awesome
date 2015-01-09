1f41ef0d0ac1a86bf62b7de9d63d020aaeaa9b62
railslauncher <railslauncher@gmail.com>
2015-01-09 15:48:35 +0530

    Add icons filter

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -3,4 +3,11 @@ $(function() {
   $('#icon-carousel').carousel({
     interval: 5000
   });
+
+  // setup icons filter
+  $('#icons-filter').hideseek({
+    highlight: true,
+    list: ".fontawesome-icon-list",
+    nodata: 'No icons found'
+  });
 });
