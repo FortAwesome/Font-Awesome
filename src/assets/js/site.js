3f5869e03745f6b03090f90744c1aa0fad5ee5d6
Dave Gandy <dave@davegandy.com>
2015-09-01 12:07:46 -0400

    Adding no results text, udpating algolia logo

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -1,64 +1,76 @@
 $(function () {
-    $("#newsletter").validate();
+  $("#newsletter").validate();
 
-    var ads = [
-        {
-            quote: "Take your icon game to the next level. Check out <strong>Fonticons</strong>, from the maker of Font Awesome.",
-            class: "fonticons",
-            url: "https://fonticons.com/?utm_source=font_awesome_homepage&utm_medium=display&utm_content=ad_1_next_level&utm_campaign=promo_4.4_update",
-            btn_text: "Gimme Some!"
-        },
-        {
-            quote: "Make your icons load 10x faster! Check out <strong>Fonticons</strong>, from the maker of Font Awesome.",
-            class: "fonticons",
-            url: "https://fonticons.com/?utm_source=font_awesome_homepage&utm_medium=display&utm_content=ad_3_faster_loading&utm_campaign=promo_4.4_update",
-            btn_text: "Gimme Some!"
-        },
-        {
-            quote: "Looking for other great icon sets? Check out <strong>Fonticons</strong>, from the maker of Font Awesome.",
-            class: "fonticons",
-            url: "https://fonticons.com/?utm_source=font_awesome_homepage&utm_medium=display&utm_content=ad_4_more_icons&utm_campaign=promo_4.4_update",
-            btn_text: "Gimme Some!"
-        },
-        {
-            quote: "Want to add your own icon? Check out <strong>Fonticons</strong>, from the maker of Font Awesome.",
-            class: "fonticons",
-            url: "https://fonticons.com/?utm_source=font_awesome_homepage&utm_medium=display&utm_content=ad_6_your_own_icon&utm_campaign=promo_4.4_update",
-            btn_text: "Gimme Some!"
-        },
+  var ads = [
+    {
+      quote: "Take your icon game to the next level. Check out <strong>Fonticons</strong>, from the maker of Font Awesome.",
+      class: "fonticons",
+      url: "https://fonticons.com/?utm_source=font_awesome_homepage&utm_medium=display&utm_content=ad_1_next_level&utm_campaign=promo_4.4_update",
+      btn_text: "Gimme Some!"
+    },
+    {
+      quote: "Make your icons load 10x faster! Check out <strong>Fonticons</strong>, from the maker of Font Awesome.",
+      class: "fonticons",
+      url: "https://fonticons.com/?utm_source=font_awesome_homepage&utm_medium=display&utm_content=ad_3_faster_loading&utm_campaign=promo_4.4_update",
+      btn_text: "Gimme Some!"
+    },
+    {
+      quote: "Looking for other great icon sets? Check out <strong>Fonticons</strong>, from the maker of Font Awesome.",
+      class: "fonticons",
+      url: "https://fonticons.com/?utm_source=font_awesome_homepage&utm_medium=display&utm_content=ad_4_more_icons&utm_campaign=promo_4.4_update",
+      btn_text: "Gimme Some!"
+    },
+    {
+      quote: "Want to add your own icon? Check out <strong>Fonticons</strong>, from the maker of Font Awesome.",
+      class: "fonticons",
+      url: "https://fonticons.com/?utm_source=font_awesome_homepage&utm_medium=display&utm_content=ad_6_your_own_icon&utm_campaign=promo_4.4_update",
+      btn_text: "Gimme Some!"
+    },
 
 
-        {
-            quote: "<strong>Black Tie</strong>, from the creator of Font Awesome. On sale at the Kickstarter price for a limited time.",
-            class: "black-tie",
-            url: "http://blacktie.io/?utm_source=font_awesome_homepage&utm_medium=display&utm_content=ad_2_kickstarter&utm_campaign=promo_4.4_update",
-            btn_text: "Check it Out!"
-        },
-        {
-            quote: "Want clean, minimalist icons? Check out <strong>Black Tie</strong>, the new multi-weight icon font from the maker of Font Awesome.",
-            class: "black-tie",
-            url: "http://blacktie.io/?utm_source=font_awesome_homepage&utm_medium=display&utm_content=ad_5_clean_minimalist&utm_campaign=promo_4.4_update",
-            btn_text: "Check it Out!"
-        }
-    ];
-
-    selectFonticonsAd();
-
-    // start the icon carousel
-    $('#icon-carousel').carousel({
-        interval: 5000
-    });
-
-    $('[data-toggle="popover"]').popover();
-
-    function selectFonticonsAd() {
-        random_number = Math.floor(Math.random() * ads.length);
-        random_ad = ads[random_number];
-
-        $('#banner').addClass(random_ad.class);
-        $('#rotating-message').html(random_ad.quote);
-        $('#rotating-url').attr("href", random_ad.url);
-        $('#rotating-url').html(random_ad.btn_text);
-        $('#banner').collapse('show');
+    {
+      quote: "<strong>Black Tie</strong>, from the creator of Font Awesome. On sale at the Kickstarter price for a limited time.",
+      class: "black-tie",
+      url: "http://blacktie.io/?utm_source=font_awesome_homepage&utm_medium=display&utm_content=ad_2_kickstarter&utm_campaign=promo_4.4_update",
+      btn_text: "Check it Out!"
+    },
+    {
+      quote: "Want clean, minimalist icons? Check out <strong>Black Tie</strong>, the new multi-weight icon font from the maker of Font Awesome.",
+      class: "black-tie",
+      url: "http://blacktie.io/?utm_source=font_awesome_homepage&utm_medium=display&utm_content=ad_5_clean_minimalist&utm_campaign=promo_4.4_update",
+      btn_text: "Check it Out!"
+    },
+    {
+      quote: "Want a different icon look? Check out <strong>Black Tie</strong>, our new multi-weight icon set.",
+      class: "black-tie",
+      url: "http://blacktie.io/?utm_source=font_awesome_homepage&utm_medium=display&utm_content=ad_6_different_look&utm_campaign=promo_4.4_update",
+      btn_text: "Check it Out!"
+    },
+    {
+      quote: "Check out <strong>Black Tie</strong>, our new multi-weight icon set!",
+      class: "black-tie",
+      url: "http://blacktie.io/?utm_source=font_awesome_homepage&utm_medium=display&utm_content=ad_7_our_new_multi_weight&utm_campaign=promo_4.4_update",
+      btn_text: "Check it Out!"
     }
+  ];
+
+  selectFonticonsAd();
+
+  // start the icon carousel
+  $('#icon-carousel').carousel({
+    interval: 5000
+  });
+
+  $('[data-toggle="popover"]').popover();
+
+  function selectFonticonsAd() {
+    random_number = Math.floor(Math.random() * ads.length);
+    random_ad = ads[random_number];
+
+    $('#banner').addClass(random_ad.class);
+    $('#rotating-message').html(random_ad.quote);
+    $('#rotating-url').attr("href", random_ad.url);
+    $('#rotating-url').html(random_ad.btn_text);
+    $('#banner').collapse('show');
+  }
 });
