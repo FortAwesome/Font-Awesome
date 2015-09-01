84090871a935fedaeb8a8f9475cd2b39e18475e4
Dave Gandy <dave@davegandy.com>
2015-09-01 14:52:06 -0400

    adding algolia search

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
   $("#newsletter").validate();
 
   var ads = [
@@ -28,7 +28,6 @@ $(function() {
     },
 
 
-
     {
       quote: "<strong>Black Tie</strong>, from the creator of Font Awesome. On sale at the Kickstarter price for a limited time.",
       class: "black-tie",
@@ -39,6 +38,18 @@ $(function() {
       quote: "Want clean, minimalist icons? Check out <strong>Black Tie</strong>, the new multi-weight icon font from the maker of Font Awesome.",
       class: "black-tie",
       url: "http://blacktie.io/?utm_source=font_awesome_homepage&utm_medium=display&utm_content=ad_5_clean_minimalist&utm_campaign=promo_4.4_update",
+      btn_text: "Check it Out!"
+    },
+    {
+      quote: "Want a different icon look? Check out <strong>Black Tie</strong>, our new multi-weight icon set.",
+      class: "black-tie",
+      url: "http://blacktie.io/?utm_source=font_awesome_homepage&utm_medium=display&utm_content=ad_6_different_look&utm_campaign=promo_4.4_update",
+      btn_text: "Check it Out!"
+    },
+    {
+      quote: "Check out <strong>Black Tie</strong>, our new multi-weight icon set!",
+      class: "black-tie",
+      url: "http://blacktie.io/?utm_source=font_awesome_homepage&utm_medium=display&utm_content=ad_7_our_new_multi_weight&utm_campaign=promo_4.4_update",
       btn_text: "Check it Out!"
     }
   ];
@@ -52,87 +63,8 @@ $(function() {
 
   $('[data-toggle="popover"]').popover();
 
-  var $filter_by = $('#filter-by');
-
-  // Filter icons
-  if($filter_by.length) {
-    var $filter_val = $('#filter-val');
-    var $filter = $('#filter');
-    var $other = $('#new, #web-application, #hand, #transportation, #gender, #form-control, #medical, #currency, #text-editor, #directional, #video-player, #brand, #file-type, #spinner, #payment, #chart');
-    var $clear = $('#filter-clear');
-    var $no_results = $('#no-search-results');
-
-    var $icons = $('.filter-icon', $filter);
-
-    // Add tab completion
-    $filter_by.tabcomplete(filterSet, {
-      arrowKeys: true
-    });
-
-    $clear.on('click', function(e) {
-      e.preventDefault();
-      $filter_by
-        .val('')
-        .trigger('input')
-        .trigger('keyup')
-        .focus();
-
-      $clear.addClass('hide'); // Hide clear button
-    });
-
-
-    $filter_by.on('keyup', function() {
-      var $this = $(this);
-      var val = $this.val().toLowerCase();
-      $filter.toggle(!!val);
-      $other.toggle(!val);
-      $clear.toggleClass('hide', !val);
-      $filter_val.text(val);
-
-      if(!val) return;
-
-      var resultsCount = 0;
-      $icons.each(function() {
-        var filter = $(this).attr('data-filter').split('|');
-        var show = inFilter(val, filter);
-        if (!show) {
-          if (val.slice(-1) === 's') {
-            // Try to be smart. Make plural terms singular.
-            show = inFilter(val.slice(0, -1), filter);
-          }
-        }
-        if (show) resultsCount++;
-        $(this).toggle(!!show);
-      });
-
-      if( resultsCount == 0 && val.length != 0 ) {
-        $no_results.find('span').text(val);
-        $no_results.show();
-      } else {
-        $no_results.hide();
-      }
-    });
-  }
-
-  function inFilter(val, filter) {
-    for (var i = 0; i < filter.length; i++) {
-      if (filter[i].match(val)) return true;
-    }
-    return false;
-  }
-
-  $filter_by
-    .val('')
-    .trigger('input')
-    .trigger('keyup')
-    .focus();
-
-  if ($clear) {
-    $clear.addClass('hide'); // Hide clear button
-  }
-
   function selectFonticonsAd() {
-    random_number = Math.floor( Math.random() * ads.length );
+    random_number = Math.floor(Math.random() * ads.length);
     random_ad = ads[random_number];
 
     $('#banner').addClass(random_ad.class);
