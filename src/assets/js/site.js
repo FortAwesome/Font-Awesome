7e259f6167c6afedb9db47a7d160f03d2a3d4dcd
Dave Gandy <dave@davegandy.com>
2017-09-27 21:36:00 -0400

    Updating banner

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -3,10 +3,10 @@ $(function () {
 
   var ads = [
     {
-      quote: "September is your last chance to get FA Pro and <strong>ALL</strong> KS rewards for just $40!",
+      quote: "September is your last chance to get FA Pro and <strong>ALL</strong> KS rewards for $40.",
       class: "fa5",
       url: "https://fontawesome.com/?utm_source=font_awesome_homepage&utm_medium=display&utm_campaign=september_last_chance&utm_content=banner",
-      btn_text: "Pre-order FA Pro! &nbsp;<i class='fas fas-external-link'></i>",
+      btn_text: "Check out FA Pro! &nbsp;<i class='fas fas-external-link'></i>",
     },
   ];
 
@@ -34,6 +34,27 @@ $(function () {
   	// Yippee! We can use localStorage awesomeness
   }
 
+  function timeLeft() {
+    var now, ksEndsAt, hoursLeft, minutesLeft, humanized;
+
+    try {
+      now = moment();
+      ksEndsAt = moment("2017-10-01 00:00:00-05");
+      hoursLeft = ksEndsAt.diff(now, "hours");
+      minutesLeft = ksEndsAt.diff(now, "minutes") - hoursLeft * 60;
+    } catch (e) {}
+
+    if (hoursLeft < 0 || minutesLeft < 0) {
+      return;
+    }
+
+    humanized = hoursLeft + " " + ((hoursLeft === 1) ? "hour" : "hours") + ", " + minutesLeft + " " + ((minutesLeft === 1) ? "minute" : "minutes");
+
+    $('#time-left-message').html('Just <span class="duration">' + humanized + '</span> left before the price goes up!');
+
+    setTimeout(timeLeft, 2000);
+  }
+
   function storageAvailable(type) {
   	try {
   		var storage = window[type],
@@ -50,6 +71,8 @@ $(function () {
   function selectAd() {
     random_number = Math.floor(Math.random() * ads.length);
     random_ad = ads[random_number];
+
+    timeLeft();
 
     $('#banner').addClass(random_ad.class);
     $('#rotating-message').html(random_ad.quote);
