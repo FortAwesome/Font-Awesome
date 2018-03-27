4df1c1c390a6d3132f813a8d1e1239979ae3bf70
robmadole <robmadole@gmail.com>
2018-03-27 16:39:07 -0500

    Release 5.0.9

--- advanced-options/use-with-node-js/fontawesome-free-brands/faHouzz.js
+++ advanced-options/use-with-node-js/fontawesome-free-brands/faHouzz.js
@@ -1,1 +1,1 @@
-module.exports = { prefix: 'fab', iconName: 'houzz', icon: [320, 512, [], "f27c", "M12.2 256L160 341.1 12.2 426.6V256M160 512l147.8-85.4V256L160 341.1V512zm0-512L12.2 85.4V256L160 170.6V0zm0 170.6L307.8 256V85.4L160 170.6z"] };
+module.exports = { prefix: 'fab', iconName: 'houzz', icon: [414, 512, [], "f27c", "M258.9 330.7H154.3V480H0V32h109.5v104.5l305.1 85.6V480H258.9V330.7z"] };
--- svg-with-js/js/fa-v4-shims.js
+++ svg-with-js/js/fa-v4-shims.js
@@ -1,5 +1,5 @@
 /*!
- * Font Awesome Free 5.0.8 by @fontawesome - https://fontawesome.com
+ * Font Awesome Free 5.0.9 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  */
 (function (global, factory) {
