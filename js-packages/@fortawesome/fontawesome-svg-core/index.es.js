299abb1fb101f4e1cfd1d2fccc461782ff84b13d
Rob Madole <robmadole@gmail.com>
2019-03-21 16:50:43 -0500

    Release 5.8.1 (#14785)

--- js-packages/@fortawesome/fontawesome-svg-core/index.es.js
+++ js-packages/@fortawesome/fontawesome-svg-core/index.es.js
@@ -1051,7 +1051,7 @@ var p = config.measurePerformance && PERFORMANCE && PERFORMANCE.mark && PERFORMA
   mark: noop$1,
   measure: noop$1
 };
-var preamble = "FA \"5.8.0\"";
+var preamble = "FA \"5.8.1\"";
 
 var begin = function begin(name) {
   p.mark("".concat(preamble, " ").concat(name, " begins"));
--- js/fontawesome.js
+++ js/fontawesome.js
@@ -1,5 +1,5 @@
 /*!
- * Font Awesome Free 5.8.0 by @fontawesome - https://fontawesome.com
+ * Font Awesome Free 5.8.1 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  */
 (function () {
@@ -1068,7 +1068,7 @@
     mark: noop$1,
     measure: noop$1
   };
-  var preamble = "FA \"5.8.0\"";
+  var preamble = "FA \"5.8.1\"";
 
   var begin = function begin(name) {
     p.mark("".concat(preamble, " ").concat(name, " begins"));
