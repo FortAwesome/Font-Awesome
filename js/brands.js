3741fd7e81aa2b6b994c80ac95b0109a1d523ca8
iamayushdas <40708551+iamayushdas@users.noreply.github.com>
2019-10-11 05:08:33 +0530

    update brand.js

--- js/brands.js
+++ js/brands.js
@@ -2,7 +2,7 @@
  * Font Awesome Free 5.11.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  */
-(function () {
+function (() => {
   'use strict';
 
   var _WINDOW = {};
