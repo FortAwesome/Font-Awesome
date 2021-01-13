fcec2d1b01ff069ac10500ac42e4478d20d21f4c
Rob Madole <robmadole@gmail.com>
2021-01-13 14:23:11 -0600

    Release 5.15.2 (#17552)

--- UPGRADING.md
+++ UPGRADING.md
@@ -8,6 +8,12 @@ As always, [submit issues](https://github.com/FortAwesome/Font-Awesome/issues/ne
 
 ---
 
+## 5.15.1 to 5.15.2
+
+There are no breaking changes in this version upgrade.
+
+---
+
 ## 5.15.0 to 5.15.1
 
 There are no breaking changes in this version upgrade.
--- js-packages/@fortawesome/free-solid-svg-icons/index.es.js
+++ js-packages/@fortawesome/free-solid-svg-icons/index.es.js
@@ -1,5 +1,5 @@
 /*!
- * Font Awesome Free 5.15.1 by @fontawesome - https://fontawesome.com
+ * Font Awesome Free 5.15.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  */
 var prefix = "fas";
