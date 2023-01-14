39e05f6fae891073cca1c96ef89150d0fc3cb391
Mikael Mantis <mikael.mantis7@gmail.com>
2023-01-13 17:32:32 -0800

    adds sharp function macro to svg-core

--- js-packages/@fortawesome/fontawesome-svg-core/import.macro.d.ts
+++ js-packages/@fortawesome/fontawesome-svg-core/import.macro.d.ts
@@ -15,6 +15,7 @@ export function brands(iconName: IconName): IconDefinition;
 export function duotone(iconName: IconName): IconDefinition;
 export function light(iconName: IconName): IconDefinition;
 export function regular(iconName: IconName): IconDefinition;
+export function sharp(iconName: IconName): IconDefinition;
 export function solid(iconName: IconName): IconDefinition;
 export function thin(iconName: IconName): IconDefinition;
 export function icon(params: IconMacroParams): IconDefinition;
