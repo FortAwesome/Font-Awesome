1135a13c0de3c5419c601e630a9f6963ca7ab842
robmadole <robmadole@users.noreply.github.com>
2023-11-30 17:04:48 +0000

    Release 6.5.1

--- js-packages/@fortawesome/fontawesome-free/js/fontawesome.js
+++ js-packages/@fortawesome/fontawesome-free/js/fontawesome.js
@@ -1,5 +1,5 @@
 /*!
- * Font Awesome Free 6.5.0 by @fontawesome - https://fontawesome.com
+ * Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2023 Fonticons, Inc.
  */
@@ -1785,7 +1785,7 @@
     mark: noop$1,
     measure: noop$1
   };
-  var preamble = "FA \"6.5.0\"";
+  var preamble = "FA \"6.5.1\"";
 
   var begin = function begin(name) {
     p.mark("".concat(preamble, " ").concat(name, " begins"));
